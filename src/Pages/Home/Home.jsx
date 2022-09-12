85301019acb78b3a5e72414c1c48864f712515e0
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-09-13 05:11:46 +0600

    initial setup complete and navbar added

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../../Components/Navbar";
+import "./HomeCSS/Home.css";
+
+const Home = () => {
+  return (
+    <div className="bg h-screen">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
