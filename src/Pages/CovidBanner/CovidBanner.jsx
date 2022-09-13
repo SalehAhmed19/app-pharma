b6aa85640ef925d901cc5a67c6e3db265a542053
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-09-13 06:17:49 +0600

    covid-banner added

--- src/Pages/CovidBanner/CovidBanner.jsx
+++ src/Pages/CovidBanner/CovidBanner.jsx
@@ -4,10 +4,10 @@ import injection from "../../Assets/images/COVID-19.png";
 const CovidBanner = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 py-10">
-      <div className="flex justify-center">
+      <div className="flex justify-end">
         <img className="w-1/2" src={injection} alt="" />
       </div>
-      <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center pr-20">
         <p>Exclusively Avalable on AppsPharma</p>
         <h2 className="text-blue-900 text-5xl font-bold">COVID-19 vaccine</h2>
         <p>
